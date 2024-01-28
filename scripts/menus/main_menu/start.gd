fc7d9343a99affeca403dfd6ad03e08702397724
Yago Lautaro Torres <yagolautarotorres@gmail.com>
2024-01-28 19:07:38 -0300

    sonidos del menu
    se agregaron sonidos a los botones del menu tanto cuando  pasa el mouse como cuando pasas de opcion por teclado

--- scripts/menus/main_menu/start.gd
+++ scripts/menus/main_menu/start.gd
@@ -5,5 +5,14 @@ func _ready():
 
 var game = preload("res://scenes/game.tscn")
 
+
+func _on_mouse_entered():
+	$HitSound.play()
+func _on_focus_entered():
+	$HitSound.play()
+
 func _pressed():
+	$AudioStreamPlayer2D.play()
+	await $AudioStreamPlayer2D.finished
 	get_node("/root/Main menu").to_scene(game)
+
